6aeb4333ba8093e727bea74b80fef35fe4dc18f8
lukeskywalkerrrr <142318861+lukeskywalkerrrr@users.noreply.github.com>
2023-08-15 22:06:50 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		for(int i = 1;i <= 20;i++) {
-			if(i % 3 == 0 & i % 5 == 0) {
+			if(i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
 				
 			}else if(i % 3 == 0){
